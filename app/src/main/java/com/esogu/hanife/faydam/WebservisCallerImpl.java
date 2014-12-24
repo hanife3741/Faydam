a63a573eb2c3fe8ad3fd41f9869b8f38d9359ff5
hanife3741 <hanife.ozdemir.37@gmail.com>
2014-12-24 19:05:39 +0200

    bilgileri properties dosyası şeklinde saklanmaya başlandı

--- app/src/main/java/com/esogu/hanife/faydam/WebservisCallerImpl.java
+++ app/src/main/java/com/esogu/hanife/faydam/WebservisCallerImpl.java
@@ -8,15 +8,45 @@ import org.ksoap2.serialization.SoapObject;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
 /**
  * Created by ASUS on 23.12.2014.
  */
 public class WebservisCallerImpl implements WebServisCaller {
-    private static final String NAMESPACE="http://www.faydam.com";
-    private static final String SERVIS_URL="http://193.109.135.67:92/Service1.asmx";
-    private static final String DOGRULA_METHOD="KulaniciVarMi";
-    private static final String SOAP_DOGRULA_ACTİON=NAMESPACE+"KullaniciVarMi";
+
+
+
+
+
+
+    private static  String NAMESPACE=null;
+    private static  String SERVIS_URL=null;
+    private static  String DOGRULA_METHOD=null;
+    private static  String SOAP_DOGRULA_ACTION=null;
     public boolean KullaniciVarMi(KullaniciVarMiInput input){
+
+
+        //properties nesnesini oluştur
+        Properties prop = new Properties();
+        //properties değerlerini /src/main/assets/app.properties dosyasından stream e oku
+        InputStream inputStream = WebservisCallerImpl.class.getClassLoader().getResourceAsStream("assets/app.properties");
+        try {//streamden properties nesnesine değerleri yükle
+            prop.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        NAMESPACE =  prop.getProperty("NAMESPACE");
+        SERVIS_URL =  prop.getProperty("SERVIS_URL");
+        DOGRULA_METHOD =  prop.getProperty("DOGRULA_METHOD");
+        NAMESPACE =  prop.getProperty("NAMESPACE");
+        SOAP_DOGRULA_ACTION=NAMESPACE+DOGRULA_METHOD;
+
         SoapObject request = new SoapObject(NAMESPACE, DOGRULA_METHOD);
         request.addProperty("KullaniciAdi", input.getKullaniciAdi());
         request.addProperty("Sifre",input.getSifre());
@@ -29,7 +59,7 @@ public class WebservisCallerImpl implements WebServisCaller {
         envelope.setOutputSoapObject(request);
         HttpTransportSE androidHttpTransport=new HttpTransportSE(SERVIS_URL);
         try {
-            androidHttpTransport.call(SOAP_DOGRULA_ACTİON,envelope);
+            androidHttpTransport.call(SOAP_DOGRULA_ACTION,envelope);
             if(envelope.bodyIn instanceof SoapObject){
                 SoapObject soapObject=(SoapObject) envelope.bodyIn;
                 KullaniciVarMiInput kullaniciVarMiResult=new Gson().fromJson((String) soapObject.getProperty(0),KullaniciVarMiInput.class);
